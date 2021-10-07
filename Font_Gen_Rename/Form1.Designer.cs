0730a789b0f62e50fc28a14d5c90da4111406b60
カラス <71928247+Hashibutogarasu@users.noreply.github.com>
2021-10-07 19:18:57 +0900

    Add files via upload

---
+++ Font_Gen_Rename/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace Font_Gen_Rename
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.OutputPathBox = new System.Windows.Forms.TextBox();
+            this.FormOKButton = new System.Windows.Forms.Button();
+            this.FormCancelButton = new System.Windows.Forms.Button();
+            this.UndoRenameButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(12, 7);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(55, 15);
+            this.OutputLabel.TabIndex = 2;
+            this.OutputLabel.Text = "出力先：";
+            // 
+            // OutputPathBox
+            // 
+            this.OutputPathBox.Location = new System.Drawing.Point(11, 25);
+            this.OutputPathBox.Name = "OutputPathBox";
+            this.OutputPathBox.Size = new System.Drawing.Size(363, 23);
+            this.OutputPathBox.TabIndex = 1;
+            // 
+            // FormOKButton
+            // 
+            this.FormOKButton.Location = new System.Drawing.Point(206, 54);
+            this.FormOKButton.Name = "FormOKButton";
+            this.FormOKButton.Size = new System.Drawing.Size(87, 23);
+            this.FormOKButton.TabIndex = 3;
+            this.FormOKButton.Text = "実行";
+            this.FormOKButton.UseVisualStyleBackColor = true;
+            this.FormOKButton.Click += new System.EventHandler(this.FormOKButton_Click);
+            // 
+            // FormCancelButton
+            // 
+            this.FormCancelButton.Location = new System.Drawing.Point(13, 54);
+            this.FormCancelButton.Name = "FormCancelButton";
+            this.FormCancelButton.Size = new System.Drawing.Size(187, 23);
+            this.FormCancelButton.TabIndex = 2;
+            this.FormCancelButton.Text = "キャンセル";
+            this.FormCancelButton.UseVisualStyleBackColor = true;
+            this.FormCancelButton.Click += new System.EventHandler(this.FormCancelButton_Click);
+            // 
+            // UndoRenameButton
+            // 
+            this.UndoRenameButton.Location = new System.Drawing.Point(299, 54);
+            this.UndoRenameButton.Name = "UndoRenameButton";
+            this.UndoRenameButton.Size = new System.Drawing.Size(75, 23);
+            this.UndoRenameButton.TabIndex = 4;
+            this.UndoRenameButton.Text = "元に戻す";
+            this.UndoRenameButton.UseVisualStyleBackColor = true;
+            this.UndoRenameButton.Click += new System.EventHandler(this.UndoRenameButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.FormCancelButton;
+            this.ClientSize = new System.Drawing.Size(386, 91);
+            this.Controls.Add(this.UndoRenameButton);
+            this.Controls.Add(this.FormCancelButton);
+            this.Controls.Add(this.FormOKButton);
+            this.Controls.Add(this.OutputPathBox);
+            this.Controls.Add(this.OutputLabel);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(402, 130);
+            this.MinimumSize = new System.Drawing.Size(402, 130);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Font Gen Renamer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.TextBox OutputPathBox;
+        private System.Windows.Forms.Button FormOKButton;
+        private System.Windows.Forms.Button FormCancelButton;
+        private System.Windows.Forms.Button UndoRenameButton;
+    }
+}
+
---
+++ Font_Gen_Rename/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Font_Gen_Rename
+{
+    public partial class Form1 : Form
+    {
+        private string default_title;
+
+        public Form1()
+        {
+            InitializeComponent();
+            default_title = this.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 確実にファイル／ディレクトリの名前を変更する
+        /// </summary>
+        /// <param name="sourceFilePath">変更元ファイルパス</param>
+        /// <param name="outputFilePath">変更後ファイルパス</param>
+        public static void Rename(string sourceFilePath, string outputFilePath)
+        {
+            var fileInfo = new FileInfo(sourceFilePath);
+
+            if (fileInfo.Attributes.HasFlag(FileAttributes.Directory))
+            {
+                RenameDirectory(sourceFilePath, outputFilePath);
+            }
+            else
+            {
+                fileInfo.MoveTo(outputFilePath);
+            }
+        }
+
+        /// <summary>
+        /// 確実にディレクトリの名前を変更する
+        /// </summary>
+        /// <param name="sourceFilePath">変更元ファイルパス</param>
+        /// <param name="outputFilePath">変更後ファイルパス</param>
+        public static void RenameDirectory(string sourceFilePath, string outputFilePath)
+        {
+            //Directory.Moveはなぜか、大文字小文字だけの変更だとエラーする
+            //なので、大文字小文字だけの変更の場合は一度別のファイル名に変更する
+            if ((String.Compare(sourceFilePath, outputFilePath, true) == 0))
+            {
+                var tempPath = GetSafeTempName(outputFilePath);
+
+                Directory.Move(sourceFilePath, tempPath);
+                Directory.Move(tempPath, outputFilePath);
+            }
+            else
+            {
+                Directory.Move(sourceFilePath, outputFilePath);
+            }
+        }
+
+        /// <summary>
+        /// 指定したファイルパスが他のファイルパスとかぶらなくなるまで"_"を足して返す
+        /// </summary>
+        private static String GetSafeTempName(string outputFilePath)
+        {
+            outputFilePath += "_";
+            while (File.Exists(outputFilePath))
+            {
+                outputFilePath += "_";
+            }
+            return outputFilePath;
+        }
+
+        /// <summary>
+        /// 指定されたパス文字列から拡張子を削除して返します
+        /// </summary>
+        public static string GetPathWithoutExtension(string path)
+        {
+            var extension = Path.GetExtension(path);
+            if (string.IsNullOrEmpty(extension))
+            {
+                return path;
+            }
+            return path.Replace(extension, string.Empty);
+        }
+
+        delegate void DelegateProcess();
+
+        private Task Font_Gen_Rename_Files(String OutputPath)
+        {
+            string[] names = Directory.GetFiles(OutputPath, "*");
+            int len = 0;
+            string filename = "";
+
+            int per = 0;
+            int filecount = names.Length;
+
+            foreach (string name in names)
+            {
+                if (name.Contains("unicode_page_"))
+                {
+                    filename = Path.GetFileName(name);
+                    len = name.Length;
+                    RenameDirectory(name, Path.GetDirectoryName(name) + "\\glyph_" + (GetPathWithoutExtension(filename.Substring(filename.Length - 6)).ToUpper() + ".png"));
+                    
+                    per++;
+                }
+
+                if (this.InvokeRequired)
+                {
+                    //別スレッドによるUI操作
+                    this.Invoke((MethodInvoker)(() => this.Text = default_title + "：" + ((per / filecount) * 100).ToString() + "%"));
+                }
+                else
+                {
+                    //UIスレッドからのUI操作
+                    this.Text = default_title + "：" + ((per / filecount) * 100).ToString() + "%";
+                }
+            }
+
+            if (this.InvokeRequired)
+            {
+                //別スレッドによるUI操作
+                this.Invoke((MethodInvoker)(() => this.Text = default_title));
+            }
+            else
+            {
+                //UIスレッドからのUI操作
+                this.Text = default_title;
+            }
+
+            return null;
+
+        }
+
+        private Task Undo(String OutputPath)
+        {
+            string[] names = Directory.GetFiles(OutputPath, "*");
+            int len = 0;
+            string filename = "";
+
+            int per = 0;
+            int filecount = names.Length;
+
+            foreach (string name in names)
+            {
+                if (name.Contains("glyph_"))
+                {
+                    filename = Path.GetFileName(name);
+                    len = name.Length;
+
+                    RenameDirectory(name, Path.GetDirectoryName(name) + "\\unicode_page_" + (GetPathWithoutExtension(filename.Substring(filename.Length - 6)).ToLower() + ".png"));
+
+                    per++;
+
+                }
+
+                if (this.InvokeRequired)
+                {
+                    //別スレッドによるUI操作
+                    this.Invoke((MethodInvoker)(() => this.Text = default_title + "：" + ((per / filecount) * 100).ToString() + "%"));
+                }
+                else
+                {
+                    //UIスレッドからのUI操作
+                    this.Text = default_title + "：" + ((per / filecount) * 100).ToString() + "%";
+                }
+
+            }
+
+            if (this.InvokeRequired)
+            {
+                //別スレッドによるUI操作
+                this.Invoke((MethodInvoker)(() => this.Text = default_title));
+            }
+            else
+            {
+                //UIスレッドからのUI操作
+                this.Text = default_title;
+            }
+
+            return null;
+
+        }
+
+
+        private async void FormOKButton_Click(object sender, EventArgs e)
+        {
+            string OutputPath = OutputPathBox.Text;
+
+            try
+            {
+                await Task.Run(() => Font_Gen_Rename_Files(OutputPath));
+            }
+            catch
+            {
+
+            }
+            Task.WaitAll();
+
+        }
+
+        private async void UndoRenameButton_Click(object sender, EventArgs e)
+        {
+            string OutputPath = OutputPathBox.Text;
+
+            try
+            {
+                await Task.Run(() => Undo(OutputPath));
+            }
+            catch
+            {
+
+            }
+            Task.WaitAll();
+        }
+
+        private void FormCancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+            Application.Exit();
+            Environment.Exit(1);
+        }
+    }
+}
